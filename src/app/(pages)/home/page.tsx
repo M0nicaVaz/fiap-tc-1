f6b04676b967a865c8f7f938d2d6c9343f216c71
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-07-09 08:20:52 -0300

    feat: project structure

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,12 @@
+// export interface indexProps {
+//   props?: unknown;
+// }
+
+export default function HomePage() {
+  return (
+    <div className={``}>
+      <h1>Test Page</h1>
+      <p>This is a test page for the transaction module.</p>
+    </div>
+  );
+}
