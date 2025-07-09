efb7464338a66d1bae66917041cf47c5a7bf17b4
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-07-09 13:35:38 -0300

    feat: button component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { Button } from '@/components';
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen pb-10 font-sans">
@@ -6,7 +10,21 @@ export default function Home() {
         <section className="text-body-600">menu </section>
         <section className="text-subtitle-600">welcome</section>
         <section>
-          <p className="text-subtitle bg-highlight">add transaction</p>
+          <p className="text-subtitle">add transaction</p>
+          <Button
+            title="Concluir transação"
+            onClick={() => console.log('oi')}
+          />
+          <Button
+            disabled
+            title="Cancelar transação"
+            onClick={() => console.log('oi')}
+          />
+          <Button
+            loading
+            title="Cancelar transação"
+            onClick={() => console.log('oi')}
+          />
         </section>
         <section className="text-caption-600">summary</section>
       </main>
