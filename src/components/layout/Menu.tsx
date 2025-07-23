93352b0015a733d30f78c6724b1244f93d5eca59
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-07-22 21:35:23 -0300

    fix: aside heights

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -23,7 +23,7 @@ export function Menu({ routes }: MenuProps) {
         aria-hidden={!isMenuOpen}
       />
       <nav
-        className={`${isMenuOpen && 'h-dvh'} fixed top-0 left-0 z-50 flex w-64 flex-col gap-3 ${isMenuOpen ? 'rounded-r-md' : 'rounded-md'} bg-white p-8 text-body-600 shadow-lg transition-transform duration-200 lg:min-h-[calc(100vh-7.5rem)] xl:static xl:flex xl:w-full xl:flex-col xl:p-6 xl:shadow-none ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} xl:pointer-events-auto xl:translate-x-0 xl:opacity-100`}
+        className={`${isMenuOpen && 'h-dvh'} fixed top-0 left-0 z-50 flex w-64 flex-col gap-3 ${isMenuOpen ? 'rounded-r-md' : 'rounded-md'} 6 bg-white p-8 text-body-600 shadow-lg transition-transform duration-200 lg:min-h-[915px] xl:static xl:flex xl:w-full xl:flex-col xl:p-6 xl:shadow-none ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} xl:pointer-events-auto xl:translate-x-0 xl:opacity-100`}
         style={{ minWidth: '200px' }}
         aria-label='Menu de navegação'
       >
