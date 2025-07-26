735803fbb38ba72452afac7f4eae96fca167cbef
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-07-26 10:30:52 -0300

    feat: IconButton stories

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -6,7 +6,7 @@ export interface IconButtonProps
   iconWeight?: IconWeight;
   transparent?: boolean;
   iconColor?: string;
-  variant?: 'small' | 'regular';
+  variant: 'small' | 'regular';
 }
 
 export function IconButton({
@@ -14,25 +14,29 @@ export function IconButton({
   iconWeight = 'regular',
   transparent = false,
   variant = 'regular',
-  iconColor = 'text-foreground-400',
+  iconColor,
   ...props
 }: IconButtonProps) {
-  const variantStyles = {
+  const baseStyles: Record<string, string> = {
+    bgColor: transparent ? 'bg-transparent' : 'bg-foreground-400',
+    disabled: 'disabled:opacity-50',
+    iconColor: transparent ? 'text-foreground-400' : 'text-foreground-000',
+  };
+
+  const variantStyles: Record<IconButtonProps['variant'], string> = {
     small: 'h-6 w-6',
     regular: 'h-10 w-10',
   };
 
   return (
     <button
-      className={`flex ${variantStyles[variant]} group cursor-pointer items-center justify-center rounded-full text-body-600 transition-colors hover:bg-highlight hover:text-white disabled:cursor-not-allowed disabled:bg-foreground-000 disabled:text-foreground-400 ${
-        transparent ? 'bg-transparent' : 'bg-foreground-400'
-      }`}
+      className={`flex ${variantStyles[variant]} group cursor-pointer items-center justify-center rounded-full text-body-600 transition-colors hover:bg-highlight hover:text-white disabled:cursor-not-allowed ${baseStyles.disabled} ${baseStyles.bgColor}`}
       {...props}
     >
       <Icon
         size={variant === 'small' ? 15 : 24}
         weight={iconWeight}
-        className={iconColor + ' transition-colors group-hover:text-white'}
+        className={`${iconColor || baseStyles.iconColor} transition-colors group-hover:text-white`}
       />
     </button>
   );
---
+++ src/stories/IconButton.stories.ts
@@ -0,0 +1,85 @@
+import { IconButton } from '@/components/ui/IconButton';
+import { TrashIcon } from '@phosphor-icons/react';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { fn } from 'storybook/test';
+
+const meta = {
+  title: 'Components/IconButton',
+  component: IconButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    icon: {
+      control: false,
+      description: 'Nome do ícone do Phosphor Icons a ser exibido no botão',
+    },
+    iconWeight: {
+      control: 'select',
+      options: ['thin', 'light', 'regular', 'bold', 'fill', 'duotone'],
+      description: 'Peso/estilo do ícone',
+    },
+    transparent: {
+      control: 'boolean',
+      description: 'Define se o botão tem fundo transparente',
+    },
+    iconColor: {
+      control: false,
+      description: 'Cor do ícone (classe CSS)',
+    },
+    variant: {
+      control: 'select',
+      options: ['small', 'regular'],
+      description: 'Tamanho do botão [small: 24x24, regular: 40x40]',
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Define se o botão está desabilitado',
+    },
+    onClick: {
+      action: 'clicked',
+      description: 'Função chamada quando o botão é clicado',
+    },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    icon: TrashIcon,
+    variant: 'regular',
+  },
+};
+export const Transparent: Story = {
+  args: {
+    icon: TrashIcon,
+    transparent: true,
+    variant: 'regular',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    icon: TrashIcon,
+    variant: 'small',
+  },
+};
+export const Disabled: Story = {
+  args: {
+    icon: TrashIcon,
+    disabled: true,
+    variant: 'regular',
+  },
+};
+
+export const Interactive: Story = {
+  args: {
+    icon: TrashIcon,
+    onClick: fn(() => alert('IconButton clicado!')),
+    variant: 'regular',
+  },
+};
