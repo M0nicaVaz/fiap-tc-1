9d1ae2a0f14add9a3415a6ae86a56d9f24cf9b73
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-08-09 10:52:29 -0300

    feat: add button variants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,12 +4,23 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   loading?: boolean;
+  variant?: 'primary' | 'secondary';
 }
 
-export function Button({ title, loading, ...props }: ButtonProps) {
+export function Button({
+  title,
+  loading,
+  variant = 'primary',
+  ...props
+}: ButtonProps) {
+  const variantStyles = {
+    primary: 'bg-foreground-400 text-white',
+    secondary: 'bg-foreground-000 text-foreground-400',
+  };
+
   return (
     <button
-      className={`relative flex w-36 cursor-pointer items-center justify-center gap-2 rounded-md bg-foreground-400 px-4 py-2 text-body-600 text-white transition-colors hover:bg-highlight disabled:cursor-not-allowed disabled:bg-foreground-000 disabled:text-foreground-400 disabled:opacity-50 sm:w-full sm:max-w-46`}
+      className={`${variantStyles[variant]} relative flex w-36 cursor-pointer items-center justify-center gap-2 rounded-md px-4 py-2 text-body-600 transition-colors hover:bg-highlight hover:text-white disabled:cursor-not-allowed disabled:bg-foreground-000 disabled:text-foreground-400 disabled:opacity-50 sm:w-full sm:max-w-46`}
       disabled={loading || props.disabled}
       {...props}
     >
